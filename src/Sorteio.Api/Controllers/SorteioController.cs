b3cc1f466a55b55c4abedc5ca0040fccaa4e08ff
caio_dom <caio_henrrique@hotmail.com>
2024-03-05 23:36:40 -0300

    controller sorteio

---
+++ src/Sorteio.Api/Controllers/SorteioController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Sorteio.Api.Controllers.Base;
+using Sorteio.Api.Models;
+using Sorteio.Business.Interfaces;
+using Sorteio.Business.Interfaces.Services;
+
+namespace Sorteio.Api.Controllers
+{
+    [Route("api/sorteio")]
+    public class SorteioController : MainController
+    {
+
+        private readonly ITicketSorteioService _ticketSorteioService;
+        private readonly IMapper _mapper;
+        public SorteioController(ITicketSorteioService ticketSorteioService,
+                                 INotificador notificador,
+                                 IMapper mapper,
+                                 IUser appUser) : base(notificador, appUser)
+        {
+            _ticketSorteioService = ticketSorteioService;
+            _mapper = mapper;
+        }
+
+        [HttpPost("sortear")]
+        public ActionResult<TicketSorteioViewModel> Sortear(Guid idDadosSorteio)
+        {
+            if (!ModelState.IsValid)
+                return CustomResponse(ModelState);
+
+            return CustomResponse(_mapper.Map<TicketSorteioViewModel>(_ticketSorteioService.Sortear(idDadosSorteio)));
+        }
+    }
+}
